b2ff7922a742b4420d6bd498eeb880a2cde67b99
hyeonic <dev.hyeonic@gmail.com>
2021-12-16 13:37:14 +0900

    feat: LottoCount 관련 검증 기능 추가

--- src/main/java/lotto/view/LottoCount.java
+++ src/main/java/lotto/view/LottoCount.java
@@ -1,9 +1,41 @@
 package lotto.view;
 
 public class LottoCount {
+    private static final int DEFAULT_LOTTO_COUNT = 0;
+    private static final int MAX_LOTTO_COUNT = 100;
+
+    private static final String VALID_NUMBER_FORMAT = "[ERROR] 로또 개수는 숫자여아 합니다.";
+    private static final String VALID_NEGATIVE_NUMBER = "[ERROR] 로또 개수는 0개 이상이어야 합니다.";
+    private static final String VALID_MAXIMUM_COUNT = "[ERROR] 로또 개수는 최대 100개 구매 가능합니다.";
+
     private final int lottoCount;
 
     public LottoCount(String inputLottoCount) {
+        validateNumberFormat(inputLottoCount);
+        int lottoCount = Integer.parseInt(inputLottoCount);
+
+        validateNegativeNumber(lottoCount);
+        validateMaximumCount(lottoCount);
         this.lottoCount = Integer.parseInt(inputLottoCount);
+    }
+
+    private void validateNumberFormat(String inputLottoCount) {
+        try {
+            Integer.parseInt(inputLottoCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(VALID_NUMBER_FORMAT);
+        }
+    }
+
+    private void validateNegativeNumber(int lottoCount) {
+        if (lottoCount < DEFAULT_LOTTO_COUNT) {
+            throw new IllegalArgumentException(VALID_NEGATIVE_NUMBER);
+        }
+    }
+
+    private void validateMaximumCount(int lottoCount) {
+        if (lottoCount > MAX_LOTTO_COUNT) {
+            throw new IllegalArgumentException(VALID_MAXIMUM_COUNT);
+        }
     }
 }
