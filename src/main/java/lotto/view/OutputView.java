0df3b32c653fe344df9cddec565a5afa3798219c
hyeonic <dev.hyeonic@gmail.com>
2021-12-16 15:51:50 +0900

    feat: OutputView 로또 결과 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,10 +2,18 @@ package lotto.view;
 
 import java.util.List;
 
+import lotto.domain.Rank;
+
 public class OutputView {
     private static final String WINNING_NUMBERS_MESSAGE_PREFIX = "금주의 당첨 번호는 ";
     private static final String DELIMITER = ", ";
     private static final String WINNING_NUMBERS_MESSAGE_SUFFIX = "입니다.";
+    private static final String LOTTO_RESULT_MESSAGE = "당신의 수익률은 아래와 같습니다.";
+    private static final String LOTTO_COUNT_FORMAT = "구매 개수: %d\n";
+    private static final String PAYMENT_FORMAT = "구매 금액: %d\n";
+    private static final String MAX_RANK_FORMAT = "최대 당첨 등수: %s\n";
+    private static final String TOTAL_REWARD_FORMAT = "총 당첨 금액: %d\n";
+    private static final String YIELD_FORMAT = "수익율: %.2f%%";
 
     private OutputView() {
     }
@@ -21,5 +29,15 @@ public class OutputView {
         stringBuilder.append(WINNING_NUMBERS_MESSAGE_SUFFIX);
 
         System.out.println(stringBuilder);
+        System.out.println();
+    }
+
+    public static void printLottoResult(int lottoCount, int payment, Rank maxRank, int totalReward, double yield) {
+        System.out.println(LOTTO_RESULT_MESSAGE);
+        System.out.printf(LOTTO_COUNT_FORMAT, lottoCount);
+        System.out.printf(PAYMENT_FORMAT, payment);
+        System.out.printf(MAX_RANK_FORMAT, maxRank.getName());
+        System.out.printf(TOTAL_REWARD_FORMAT, totalReward);
+        System.out.printf(YIELD_FORMAT, yield);
     }
 }
