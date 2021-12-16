46298e33136039330e82c30cc38e9f4ac30a989b
hyeonic <dev.hyeonic@gmail.com>
2021-12-16 13:26:43 +0900

    feat: 구매할 로또 개수를 관리하는 LottoCount 추가

---
+++ src/main/java/lotto/view/LottoCount.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class LottoCount {
+    private final int lottoCount;
+
+    public LottoCount(String inputLottoCount) {
+        this.lottoCount = Integer.parseInt(inputLottoCount);
+    }
+}
