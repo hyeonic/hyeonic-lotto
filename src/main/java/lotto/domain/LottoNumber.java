63e7ffc51f4e75fa3dfd0611e40c1a81d08452d8
hyeonic <dev.hyeonic@gmail.com>
2021-12-16 15:20:33 +0900

    feat: LottoNumber 비교를 위한 equals, hashCode 메서드 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
     private final int lottoNumber;
 
@@ -10,5 +12,22 @@ public class LottoNumber {
     @Override
     public String toString() {
         return String.valueOf(lottoNumber);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber)o;
+        return lottoNumber == that.lottoNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumber);
     }
 }
