6a1e366306372f7ec6e102359189efba5670e932
hyeonic <dev.hyeonic@gmail.com>
2021-12-16 13:41:40 +0900

    feat: LottoController 예외 발생 시 재입력 하도록 기능 수정

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -2,10 +2,20 @@ package controller;
 
 import lotto.view.InputView;
 import lotto.view.LottoCount;
+import lotto.view.OutputView;
 
 public class LottoController {
 
     public void run() {
-        LottoCount lottoCount = new LottoCount(InputView.lottoCount());
+        getLottoCount();
+    }
+
+    private void getLottoCount() {
+        try {
+            LottoCount lottoCount = new LottoCount(InputView.lottoCount());
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            getLottoCount();
+        }
     }
 }
