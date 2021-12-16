bc3aaa72f48de433103d1b8342f2f01f6b32f637
hyeonic <dev.hyeonic@gmail.com>
2021-12-16 13:52:38 +0900

    feat: InputView 구입 금액 입력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,12 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_LOTTO_COUNT_MESSAGE = "구매할 로또의 개수를 입력해주세요.";
+    private static final String INPUT_PRICE_MESSAGE_FORMAT = "금액은 %d원 입니다. 구입 금액을 입력해주세요.\n";
 
     private InputView() {
     }
 
     public static String lottoCount() {
         System.out.println(INPUT_LOTTO_COUNT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String payment(int price) {
+        System.out.printf(INPUT_PRICE_MESSAGE_FORMAT, price);
         return Console.readLine();
     }
 }
