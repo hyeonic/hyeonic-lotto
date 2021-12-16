45768f20ec771a5e461dc4be474ddc4023be4bef
hyeonic <dev.hyeonic@gmail.com>
2021-12-16 13:39:32 +0900

    feat: 출력을 위한 OutputView 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+public class OutputView {
+
+    private OutputView() {
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.println(message);
+        System.out.println();
+    }
+}
